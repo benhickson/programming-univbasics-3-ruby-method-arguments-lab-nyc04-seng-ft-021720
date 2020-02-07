28ee2e271144fe2896bdbcf553618d2707f4b766
Ben Hickson <bzhickson@gmail.com>
2020-02-07 04:09:23 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,6 +5,18 @@
 
 
 
+def introduction(name) do
+  puts "Hi, my name is #{name}"
+end
+
+def introduction_with_language(name, language) do
+  puts "Hi my name is #{name} and I am learning to program in #{language}."
+end
+
+def introduction_with_language_optional(name, language = 'Ruby') do
+  puts "Hi my name is #{name} and I am learning to program in #{language}."
+end
+
 
 
 
