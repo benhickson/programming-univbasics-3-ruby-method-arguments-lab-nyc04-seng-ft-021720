ba3993b4529a1996166d621df81af661c97621de
Ben Hickson <bzhickson@gmail.com>
2020-02-07 04:10:05 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,32 +5,32 @@
 
 
 
-def introduction(name) do
+def introduction(name)
   puts "Hi, my name is #{name}"
 end
 
-def introduction_with_language(name, language) do
-  puts "Hi my name is #{name} and I am learning to program in #{language}."
-end
-
-def introduction_with_language_optional(name, language = 'Ruby') do
-  puts "Hi my name is #{name} and I am learning to program in #{language}."
-end
-
-
-
-
-
-
-
-def introduction(name)
-  puts "Hi, my name is #{name}."
-end
-
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  puts "Hi my name is #{name} and I am learning to program in #{language}."
 end
 
-def introduction_with_language_optional(name, language="Ruby")
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+def introduction_with_language_optional(name, language = 'Ruby')
+  puts "Hi my name is #{name} and I am learning to program in #{language}."
 end
+
+
+
+
+
+
+
+# def introduction(name)
+#   puts "Hi, my name is #{name}."
+# end
+
+# def introduction_with_language(name, language)
+#   puts "Hi, my name is #{name} and I am learning to program in #{language}."
+# end
+
+# def introduction_with_language_optional(name, language="Ruby")
+#   puts "Hi, my name is #{name} and I am learning to program in #{language}."
+# end
